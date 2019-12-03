8a79e05a57f516a7093121df63cb317fcfdd55d7
dragontototro <dragonribot@gmail.com>
2019-12-04 01:36:48 +0900

    me

---
+++ src/users/users.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, UseGuards, Get, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('users')
+export class UsersController {
+    @UseGuards(AuthGuard('jwt'))
+    @Get('me')
+    getMe(@Request() req: any) {
+        return req.user;
+    }
+}
